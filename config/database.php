fbdddfc7ed3b7a887cd59844435a56f4532d9ef7
ahmdsfdn <saifudinahmad439@gmail.com>
2025-01-24 13:03:40 +0700

    sql mode

--- config/database.php
+++ config/database.php
@@ -62,11 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'modes' => [
-                'STRICT_ALL_TABLES',
-                'ERROR_FOR_DIVISION_BY_ZERO',
-                'NO_ZERO_DATE',
-                'NO_ZERO_IN_DATE',
-                'NO_AUTO_CREATE_USER',
+                "STRICT_TRANS_TABLES","NO_ZERO_IN_DATE","NO_ZERO_DATE","ERROR_FOR_DIVISION_BY_ZERO","NO_ENGINE_SUBSTITUTION"
             ],
         ],
 
